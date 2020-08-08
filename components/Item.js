00156bf4c7fafd9005a058be2714e46d7268b772
gandhamvaresh <gandhamvaresh@gmail.com>
2020-08-08 16:50:28 +0530

    2 files modified useeffect

--- components/Item.js
+++ components/Item.js
@@ -1,3 +1,13 @@
-import React from 'react';
+import React, {useState , Component } from 'react';
+  const initList = [
+  { name: "tomato", calorie: 20 },
+  { name: "rice", calorie: 340 },
+  { name: "candys", calorie: 10 }, { name: "ricee", calorie: 80 },
+  { name: "candy", calorie: 170 }
+];
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+function item(){
+
+}
+
+export default item;
--- index.js
+++ index.js
@@ -1,73 +1,44 @@
-import React, {useState , Component } from 'react';
-import { render } from 'react-dom';
-import Item from "./components/Item";
-import UserItem from "./components/UserItem";
-import useList from "./hooks/useList";
-// import ControlledForms from "./components/forms"
-import UseRefEffect from "./components/useRefEffect"
+import React, { useState, useEffect, Component } from "react";
+import { render } from "react-dom";
+import "./css/style.css";
 
-import './css/style.css';
+const born = false;
+function App() {
+  const [num, setNum] = useState(0);
+  const [achi, setachi] = useState(false);
 
+  useEffect(() => {
+    console.log("I am first call");
+  }, []);
 
-  const initList = [
-  { name: "tomato", calorie: 20 },
-  { name: "rice", calorie: 340 },
-  { name: "candys", calorie: 10 }, { name: "ricee", calorie: 80 },
-  { name: "candy", calorie: 170 }
-];
+    useEffect(() => {
+      if(born){
+        document.title="you achived";
+      }
+  },[achi]);
 
+  useEffect(() => {
+    if(born){
+      console.log("every : your calling now");
+     }else{
+       console.log("every : your calling and restricting");
+       born = true;
+     }
+    if(num > 50){
+       setachi(true);
+     }else{
+      setachi(false)}
+  });
 
-function App(){
-    // const [list, setList ] = useState(initList);
-    const item = useList(initList)
-    const [editable, setEditable ] = useState(false);
-    
-    
- function removeItemHandle(){
-    // console.dir(e.target.name);
-    // let filteredList = list.filter(v => v.calorie < 50 );
-    item.removeByFilter();
-    // setList(filteredList);
-};
-
-function removeThisItemHandle(e){
-console.dir(e.target);
-item.removeItem(e.target.name);
-//  let  filteredList = list.filter(v => v.name !== e.target.name );
-//  setList(filteredList);
-};
-
-function editableThisItemHandle(){
- setEditable(true);
+  function clickHandle() {
+    setNum(num + 10);
+  }
+  return (
+    <div>
+      Number Is:  {num}<br/><br/><br/>
+      <button onClick={clickHandle}> add </button>
+    </div>
+  );
 }
 
-function keyPressHandler(e,i){
-if(e.key === 'Enter'){
-  item.addItem( e.target.value, i)
-//   let copylist = [...list]
-// copylist[i].name = e.target.value                                      
-setEditable(!editable);
-}
-}
-
-    return (
-      <div>
- {/*     <ControlledForms > */}
- <UseRefEffect />
-        <Item />
-        {item.list.map((k,i)=>{ return <UserItem key={i} user={k} 
-                                        onClick={removeThisItemHandle}
-                                        editable={editable}
-                                       onDoubleClick={editableThisItemHandle}
-                                       onKeyPress={keyPressHandler}  
-                                       index={i}/>})}
-        <button onClick={removeItemHandle}  >adf </button>
-        <p>
-          Start editing to see some magic happen  :)
-        </p>
-      </div>
-    );
-  
-}
-
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
